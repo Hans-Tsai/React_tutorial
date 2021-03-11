7fd8a62af8eac8c9cb72dc07aec3c340c7c96e2e
Hans <lgs840522@gmail.com>
2021-03-11 12:15:51 +0800

    feat(client): add Declaring a Winner chapter(use the helper function: calculateWinner(squares))

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,10 +23,13 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X': 'O';
-    squares[i] = 'X';
-    this.setState({squares: squares,
-                  xIsNext: !this.state.xIsNext,
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
     });
   }
 
@@ -40,7 +43,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext? 'X': 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner' + winner;
+    } else {
+      status = 'Next Player' + (this.state.xIsNext ? 'X': 'O');
+    }
 
     return (
       <div>
@@ -82,3 +91,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// 這個helper function: calculateWinner()
+// 給予這個function一個含有九個 square 的 array，它將會檢查贏家並根據需要回傳 'X'、'O' 或是 null。
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a]&&squares[a] === squares[b]&&squares[b] === squares[c]===squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
