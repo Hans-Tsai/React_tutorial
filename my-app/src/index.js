0c608041915ff4458383ab91247f311c0d78dd82
Hans <lgs840522@gmail.com>
2021-03-11 11:08:55 +0800

    feat(client): add Passing Data Through Props chapter

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Square component會render一個Button
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
 }
 
+// Board component會render九個方格
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
@@ -43,6 +45,7 @@ class Board extends React.Component {
   }
 }
 
+// Game component會render一個完整的遊戲格盤＆placeholder
 class Game extends React.Component {
   render() {
     return (
